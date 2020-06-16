e9d4fb18d5cda0c3207ca70045da5999b5be9ac2
RashikAnsar <sk.ansar46@gmail.com>
2020-06-16 15:42:23 +0530

    extracting substrings

---
+++ extracting_substrings/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	greetings := "Hello, World and Universe"
+	helloWorld := greetings[:12]
+
+	fmt.Println(helloWorld)
+}
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 # Go
 
 1. [Trimming Spaces](./trimming_spaces/main.go)
+2. [Extracting substrings](./extracting_substrings/main.go)
