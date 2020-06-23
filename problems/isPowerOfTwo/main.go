bf2068b7dd3fd99bb5bd230105ef254308773fa9
RashikAnsar <sk.ansar46@gmail.com>
2020-06-23 11:16:23 +0530

    updated to bitwise operation in Is Power Of Two problem

--- problems/isPowerOfTwo/main.go
+++ problems/isPowerOfTwo/main.go
@@ -1,16 +1,18 @@
 package main
 
 func isPowerOfTwo(x int) bool {
-	if x < 1 {
-		return false
-	}
-	for x != 1 {
-		if x%2 != 0 {
-			return false
-		}
-		x = x / 2
-	}
-	return true
+	// if x < 1 {
+	// 	return false
+	// }
+	// for x != 1 {
+	// 	if x%2 != 0 {
+	// 		return false
+	// 	}
+	// 	x = x / 2
+	// }
+	// return true
+	// // using bitwise operations
+	return x&(x-1) == 0
 }
 
 func main() {}
